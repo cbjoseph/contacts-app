71f7fb12b0839afddfee40f8994f09d936ea89c1
Caroline Joseph <carbroj@gmail.com>
2016-07-10 14:11:56 -0500

    added form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,13 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
     render 'contactlist.html.erb'
   end
+
+  def add_contact
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @email = params[:email]
+    @phone_number = params[:phone_number]
+    render 'form_result.html.erb'
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/contacts' => 'contacts#contact_list'
+  post '/form_result' => 'contacts#add_contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
